0087b73d13787c57aea1b9ccfeb33bcfc2989878
Михаил <perchikxd555@yandex.ru>
2025-03-24 13:07:35 +0300

    Mars 3

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for
+from json import load
 
 app = Flask(__name__)
 
 
-@app.route('/index/<title>')
+@app.route('/<title>')
 def index(title):
     return render_template('base.html', title=title)
+
+
+@app.route('/list_prof/<mode>')
+def list_prof(mode):
+    with open('data/professions.json', encoding='utf8') as input_json:
+        prof_mas = load(input_json)['professions']
+    print(prof_mas)
+    return render_template('list_professions.html', title='professions', massive=prof_mas, mode=mode)
 
 
 if __name__ == '__main__':
